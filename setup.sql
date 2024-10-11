a3e025312d90527cfdf777873b917efd83b4d8d8
Maksym Shyiko <maxymshyjko@gmail.com>
2024-10-11 11:17:38 +0100

    Updated setup.sql

--- setup.sql
+++ setup.sql
@@ -71,29 +71,24 @@ CREATE TABLE IF NOT EXISTS account_auction (
                                                    REFERENCES auction(id)
 );
 
-
--- Insert a basic account
-INSERT INTO account (name, email, password) VALUES ('John Doe', 'john.doe@example.com', 'password123');
-INSERT INTO account (name, email, password) VALUES ('Jane Smith', 'jane.smith@example.com', 'password456');
-
 -- Insert a basic auction
 INSERT INTO auction (name, description, start_timestamp, end_timestamp)
 VALUES ('Cool Auction', 'Auction of Ukrainian items', '2024-10-15 10:00:00', '2024-10-15 13:00:00');
 
 -- Insert a lot linked to the auction
 INSERT INTO lot (name, image_url, start_price, order_num, auction_id)
-VALUES ('Raspberry Jam', 'https://res.cloudinary.com/dewzwcjc4/image/upload/v1728599049/Jam_wpqhng.jpg', 35.00, 1, 1);
+VALUES ('Raspberry Jam', 'images/lots/Jam.jpg', 35.00, 1, 1);
 INSERT INTO lot (name, image_url, start_price, order_num, auction_id)
-VALUES ('Original Embroidery Shirt 1960th', 'https://res.cloudinary.com/dewzwcjc4/image/upload/v1728599050/Vyshyvanka_mpdxl8.jpg', 80.00, 1, 1);
+VALUES ('Original Embroidery Shirt 1960th', 'images/lots/Vyshyvanka.jpg', 80.00, 1, 1);
 
 INSERT INTO lot (name, image_url, start_price, order_num, auction_id)
-VALUES ('Marble Statue with flower pot', 'https://res.cloudinary.com/dewzwcjc4/image/upload/v1728599049/Statue_u2bza6.jpg', 110.00, 1, 1);
+VALUES ('Marble Statue with flower pot', 'images/lots/Statue.jpg', 110.00, 1, 1);
 
 INSERT INTO lot (name, image_url, start_price, order_num, auction_id)
-VALUES ('Traditional Cherry Liquor Set', 'https://res.cloudinary.com/dewzwcjc4/image/upload/v1728599049/Vyshnia_jy9jao.jpg', 90.00, 1, 1);
+VALUES ('Traditional Cherry Liquor Set', 'images/lots/Vyshnia.jpg', 90.00, 1, 1);
 
 INSERT INTO lot (name, image_url, start_price, order_num, auction_id)
-VALUES ('Artillery Shell Vase', 'https://res.cloudinary.com/dewzwcjc4/image/upload/v1728599049/Artillery_znnqnb.jpg', 160.00, 1, 1);
+VALUES ('Artillery Shell Vase', 'images/lots/Artillery.jpg', 160.00, 1, 1);
 
 INSERT INTO lot (name, image_url, start_price, order_num, auction_id)
-VALUES ('Premium Ukrainian Chocolate', 'https://res.cloudinary.com/dewzwcjc4/image/upload/cld-sample-5.jpg', 30.00, 1, 1);
+VALUES ('Premium Ukrainian Chocolate', 'images/lots/Chocolate.jpg', 30.00, 1, 1);
